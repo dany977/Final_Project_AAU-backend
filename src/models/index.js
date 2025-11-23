fb29ce4dffe287c837ff656268c00ce52f05081d
daniel <danielalemu33@gmail.com>
2025-11-23 08:03:39 +0300

    Fix sequelize export for Render

--- src/models/index.js
+++ src/models/index.js
@@ -1,13 +1,15 @@
 
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
-  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 
