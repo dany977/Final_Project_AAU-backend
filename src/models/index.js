e52ea399564e2812f52aa64bb5f54627f41056f6
daniel <danielalemu33@gmail.com>
2025-11-23 07:32:55 +0300

    Fix database URL connection

--- src/models/index.js
+++ src/models/index.js
@@ -1,19 +1,15 @@
 
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+
 dotenv.config();
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5433,
-    dialect: "postgres",
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+});
+
 
 import UserModel from "./User.js";
 import FarmModel from "./Farm.js";
