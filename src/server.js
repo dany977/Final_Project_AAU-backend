edec63b96bf9f45ab8a0d283ee883d796b7f6e58
daniel <danielalemu33@gmail.com>
2025-11-23 19:06:49 +0300

    Fix API baseURL for production

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "https://final-project-aau-frontend.vercel.app",
+  origin: "https://final-project-aau-frontend.vercel.app/",
   credentials: true,
 }));
 
