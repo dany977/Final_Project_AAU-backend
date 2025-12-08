f95c7787ebef98b5643f58f0204af9992a207b2b
daniel alemu <danie@DESKTOP-AKI1CR5.moa.gov.et>
2025-12-08 13:55:25 +0300

    Fix CORS origins

--- src/server.js
+++ src/server.js
@@ -13,7 +13,8 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-origin: "https://final-project-aau-frontend.vercel.app",
+origin: "final-project-aau-frontend.vercel.app",
+
 credentials: true,
 })
 );
