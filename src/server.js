c20122b1656fb0c1704079a8eef97e61ffc1ed1b
daniel alemu <danie@DESKTOP-AKI1CR5.moa.gov.et>
2025-12-09 08:47:46 +0300

    server

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-origin: "https://frontends-dun-six.vercel.app",
+origin: "https://final-aau-frontends.vercel.app",
 
 credentials: true,
 })
