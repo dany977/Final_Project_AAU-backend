835224b889ae04ac0d8477a7a5c3a0c87d2621f1
daniel alemu <danie@DESKTOP-AKI1CR5.moa.gov.et>
2025-12-16 14:34:12 +0300

    Fix user table schema mismatch

--- src/server.js
+++ src/server.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 10000;
+await sequelize.sync({ alter: true });
 
 sequelize.authenticate()
   .then(() => {
